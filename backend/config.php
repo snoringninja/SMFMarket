9330f310403cf5f42cc944d8e5fa175cdc150968
xNifty <ryan@ryanmalacina.com>
2018-08-04 20:09:49 -0400

    Beginning SMF integration

--- backend/config.php
+++ backend/config.php
@@ -3,10 +3,32 @@
  * @author SnoringNinja
  * @site https://snoring.ninja
  */
+
+/*
+ * The root directory
+ */
+
+/*
+ * Database Connection Information
+ * $DBServer : The database server, typically localhost
+ * $DBUser   : The database username
+ * $DBPass   : The database password
+ * $DBName   : The database name
+ */
 $DBServer = 'localhost';
 $DBUser = 'root';
 $DBPass = '';
 $DBName = 'smfmarket';
 
+/*
+ * Pagination settings
+ * $page_start : The first page of the pagination listing
+ * $page_limit : The total number of offers to show per page
+ */
 $page_start = 1;
 $page_limit = 5;
+
+/*
+ * Forum Directory - Entire Path, we'll strip as needed later
+ */
+$forum_root = '/forum';
--- index.php
+++ index.php
@@ -3,20 +3,34 @@
  * @author SnoringNinja
  * @site https://snoring.ninja
  */
+define('PROJECT_ROOT', __DIR__.'./');
+
+include(__DIR__.'/backend/config.php');
 require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/functions/database.php';
 
+require_once(PROJECT_ROOT . '/../' . $forum_root . '/SSI.php');
+
 $loader = new Twig_Loader_Filesystem(__DIR__.'/views');
+
+$ssi_logout_function = new Twig_SimpleFunction('ssi_logout_custom', function () {
+    ssi_logout();
+});
+
 $twig = new Twig_Environment($loader, [
-    'cache' => __DIR__.'/cache',
+    //'cache' => __DIR__.'/cache',
     'cache' => false,
 ]);
+
+$twig->addFunction($ssi_logout_function);
 
 try {
     echo $twig->render('index.twig', [
         'name'         => '',
         'title'        => 'SMF Market',
         'market_items' => Database::displayCurrentEntries(),
+        'user_name' => $context['user']['username'],
+        'is_guest' => $context['user']['is_guest'],
     ]);
 } catch (Twig_Error_Loader $e) {
 } catch (Twig_Error_Runtime $e) {
